2534dfcae655529d3d33dcb67167945745a616f6
Ruben Africano <81010248+Africano19@users.noreply.github.com>
2023-05-17 10:25:10 +0100

    .

--- TechHelm/app/src/main/java/com/example/techhelm/ui/statistics/StatisticsFragment.java
+++ TechHelm/app/src/main/java/com/example/techhelm/ui/statistics/StatisticsFragment.java
@@ -4,28 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.techhelm.R;
 import com.example.techhelm.databinding.FragmentStatisticsBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class StatisticsFragment extends Fragment {
 
     private FragmentStatisticsBinding binding;
+    private GoogleMap mMap;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         StatisticsViewModel statisticsViewModel =
                 new ViewModelProvider(this).get(StatisticsViewModel.class);
-
         binding = FragmentStatisticsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textStatistics;
-        statisticsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
+            getChildFragmentManager().beginTransaction().replace(R.id.container, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+                LatLng sydney = new LatLng(-34, 151);
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            }
+        });
+
         return root;
     }
 
--- TechHelm/app/src/main/java/com/example/techhelm/ui/statistics/StatisticsViewModel.java
+++ TechHelm/app/src/main/java/com/example/techhelm/ui/statistics/StatisticsViewModel.java
@@ -4,13 +4,17 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.maps.GoogleMap;
+
 public class StatisticsViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
+
     public StatisticsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is Statistics fragment");
+
     }
 
     public LiveData<String> getText() {
