40edef47901dd2d99c23da930d1a6df4f1e9cde5
Ruben Africano <81010248+Africano19@users.noreply.github.com>
2023-05-31 18:12:10 +0100

    po

--- API/registar.php
+++ API/registar.php
@@ -1,10 +1,9 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET");
 
-// Verifica se todos os campos necessários estão presentes
-if (!isset($_POST['nome']) || !isset($_POST['email']) || !isset($_POST['telemovel']) || !isset($_POST['password'])) {
+if (!isset($_GET['nome']) || !isset($_GET['email']) || !isset($_GET['telemovel']) || !isset($_GET['password'])) {
     http_response_code(400);
     echo json_encode(['error' => 'Missing required fields']);
     exit;
@@ -16,7 +15,6 @@ $user = 'rubenpas_sp53';
 $pass = 'Password0524*';
 $charset = 'utf8mb4';
 
-// Cria uma conexão com o banco de dados
 $mysqli = new mysqli($host, $user, $pass, $db);
 if ($mysqli->connect_error) {
     http_response_code(500);
@@ -24,18 +22,15 @@ if ($mysqli->connect_error) {
     exit;
 }
 
-// Obtém os dados enviados pelo formulário
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$telemovel = $_POST['telemovel'];
-$password = $_POST['password'];
+$nome = $_GET['nome'];
+$email = $_GET['email'];
+$telemovel = $_GET['telemovel'];
+$password = $_GET['password'];
 
-// Prepara a declaração SQL e insere os dados no banco de dados
 $stmt = $mysqli->prepare("INSERT INTO utilizadores (Nome, Email, Telemovel, Password) VALUES (?, ?, ?, ?)");
 $stmt->bind_param("ssss", $nome, $email, $telemovel, $password);
 $stmt->execute();
 
-// Verifica se o registro foi inserido com sucesso
 if ($stmt->affected_rows > 0) {
     $id = $stmt->insert_id;
     echo json_encode(['id' => $id]);
@@ -44,7 +39,6 @@ if ($stmt->affected_rows > 0) {
     echo json_encode(['error' => 'Failed to register user']);
 }
 
-// Fecha a declaração e a conexão com o banco de dados
 $stmt->close();
 $mysqli->close();
 ?>
